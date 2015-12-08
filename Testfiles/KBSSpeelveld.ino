dadc5d93e45214096fd379f5067c5ce4206891de
Danny <dzmen@hotmail.com>
2015-12-08 13:54:03 +0100

    Edit test file gamefield

--- Testfiles/KBSSpeelveld.ino
+++ Testfiles/KBSSpeelveld.ino
@@ -4,34 +4,62 @@
 #include <GraphicsLib.h>
 #include <MI0283QT9.h>
 #include <String.h>
+#include <Wire.h>
+#include "ArduinoNunchuk.h"
 #include "gamefield.h"
 #include "car.h"
+
+volatile uint16_t teller = 0;
+volatile uint8_t speed = 0;
+volatile uint8_t seconden = 0;
+volatile uint8_t changeroad = 0;
+
+ISR(TIMER2_OVF_vect) {
+	teller++;
+	speed++;
+	if ( speed >= 1)
+	{
+		changeroad = 1;
+		speed = 0;
+	}
+	if ( teller >= 300)
+	{
+		seconden++;
+		teller = 0;
+	}
+}
+
 
 int main()
 {
 	init();
 	MI0283QT9 lcd;
+	TCCR2B |= (1 << CS22) | (1<<CS20);
+	TIMSK2 |= (1<<TOIE2);
+	TCNT2 = 0;
+	sei();
 	
 	lcd.begin();
 	
-	lcd.fillScreen(RGB(153,204,255));
-	
 	Gamefield veld;
-	veld.DrawRoad(lcd, 0, 2);
-	veld.DrawRoad(lcd, 1, 2);
-	veld.DrawRoad(lcd, 2, 3);
-	veld.DrawRoad(lcd, 3, 4);
-	veld.DrawRoad(lcd, 4, 4);
-	veld.DrawRoad(lcd, 5, 4);
-	veld.SetTimer(lcd, 100);
+	veld.StartRoad(lcd);
+	veld.SetTimer(0);
 	veld.SetHS(lcd, 1000);
 	veld.DrawHS(lcd);
 	veld.DrawMenu(lcd);
 	Car testautotje = Car(lcd);
-
+	uint8_t pos = 4;
+	uint8_t dir = 1;
 	while (1)
 	{
-		
+			if (changeroad)
+			{
+				veld.Generate(lcd);
+				changeroad = 0;
+			}
+			veld.SetTimer(seconden);
+			
+			
 	}
 	
 	return 0;
