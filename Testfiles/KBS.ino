c0838b64fedab6a09d574eb22d31844cfe3e15a8
TheDutchCat <whdenbesten@gmail.com>
2015-12-08 13:39:53 +0100

    Updated testfile KBS.ino

--- Testfiles/KBS.ino
+++ Testfiles/KBS.ino
@@ -5,6 +5,7 @@
 #include <MI0283QT9.h>
 #include <String.h>
 #include "button.h"
+#include "game.h"
 
 int main()
 {
@@ -14,14 +15,43 @@ int main()
 	
 	lcd.begin();
 	
-	lcd.fillScreen(RGB(153,204,255));
+	lcd.touchStartCal();
 	
-	Button start = Button(lcd,40, "Start");
-	Button highscores = Button(lcd,100, "Highscores");
-	Button help = Button(lcd,160, "Help");
-	
+	//lcd.fillScreen(RGB(153,204,255));
+	//
+	//Button start = Button(40, "Start");
+	//Button highscores = Button(100, "Highscores");
+	//Button help = Button(160, "Help");
+	//
+	//start.drawButton(lcd);
+	//highscores.drawButton(lcd);
+	//help.drawButton(lcd);
+	Game game = Game(lcd);
 	while (1)
 	{
+		game.run();
+		
+		//lcd.touchRead();
+		//
+		//int a = lcd.touchX();
+		//int b = lcd.touchY();
+		//
+		////if(a <= 20 && b <= 20)
+		////{
+			////lcd.drawChar(0,0,'a',RGB(0,0,0),RGB(153,204,255),2);
+		////}
+		//if (start.isPressed(a,b) == 1)
+		//{
+			//lcd.drawChar(0,0,'a',RGB(0,0,0),RGB(153,204,255),2);
+		//}
+		//if (highscores.isPressed(a,b) == 1)
+		//{
+			//lcd.drawChar(0,0,'b',RGB(0,0,0),RGB(153,204,255),2);
+		//}
+		//if (help.isPressed(a,b)== 1)
+		//{
+			//lcd.drawChar(0,0,'c',RGB(0,0,0),RGB(153,204,255),2);
+		//}
 		
 	}
 	
