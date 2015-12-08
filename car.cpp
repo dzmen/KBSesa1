63063588c69e3da9e8e729ed51fa24135601e168
jarnovdberg <jarno_vdberg@hotmail.com>
2015-12-08 13:14:03 +0100

    Car V2.0
    +added libraries
    * edited car files, car can move now at different speeds

--- car.cpp
+++ car.cpp
@@ -7,22 +7,62 @@
 Car::Car(MI0283QT9 lcd)
 {
 	//Draw frame
-	lcd.fillRect(140, 140, 40, 70, RGB(0,0,255));
-	
-	//Draw bumper
-	lcd.fillRect(138, 135, 44, 5, RGB(0,0,255));
+	lcd.fillRect(170, 150, 20, 60, RGB(0,0,255));
 	
 	//Draw spoiler
-	lcd.fillRect(130, 210, 60, 10, RGB(0,0,255));
+	lcd.fillRect(160, 210, 40, 10, RGB(0,0,255));
 	
 	//Draw wheels
-	lcd.fillRect(130, 145, 10, 15, RGB(0,0,0)); //left front
-	lcd.fillRect(180, 145, 10, 15, RGB(0,0,0)); //right front
-	lcd.fillRect(130, 185, 10, 15, RGB(0,0,0)); //left rear
-	lcd.fillRect(180, 185, 10, 15, RGB(0,0,0)); //right rear
-	
-	//Draw cockpit
-	lcd.fillRect(140, 170, 40, 30, RGB(0, 128, 255));
+	lcd.fillRect(160, 160, 10, 15, RGB(0,0,0)); //left front
+	lcd.fillRect(190, 160, 10, 15, RGB(0,0,0)); //right front
+	lcd.fillRect(160, 190, 10, 15, RGB(0,0,0)); //left rear
+	lcd.fillRect(190, 190, 10, 15, RGB(0,0,0)); //right rear
 }
 
+void Car::Left(MI0283QT9 lcd, uint16_t xL, uint8_t widthL)
+{
+	//Frame
+	lcd.fillRect(xL - 10, 150, widthL, 60, RGB(0,0,255));
+	
+	//Spoiler
+	lcd.fillRect(xL - 20, 210, widthL, 10, RGB(0,0,255));
+	
+	//Road
+	lcd.fillRect(xL + 10, 150, widthL, 60, RGB(160,160,160));
+	
+	//Wheels
+	lcd.fillRect(xL - 20, 160, widthL, 15, RGB(0,0,0)); //left front
+	lcd.fillRect(xL + 10, 160, widthL, 15, RGB(0,0,0)); //right front
+	lcd.fillRect(xL - 20, 190, widthL, 15, RGB(0,0,0)); //left rear
+	lcd.fillRect(xL + 10, 190, widthL, 15, RGB(0,0,0)); //right rear
+	
+	//Road
+	lcd.fillRect(xL + 20, 160, widthL, 15, RGB(160,160,160));
+	lcd.fillRect(xL + 20, 190, widthL, 15, RGB(160,160,160));
+	lcd.fillRect(xL + 20, 210, widthL, 10, RGB(160,160,160));
+}
 
+void Car::Right(MI0283QT9 lcd, uint16_t xR, uint8_t widthR)
+{
+	//Frame
+    lcd.fillRect(xR + 10, 150, widthR, 60, RGB(0,0,255));
+	
+	//Spoiler
+	lcd.fillRect(xR + 20, 210, widthR, 10, RGB(0,0,255));
+	
+	//Road	
+	lcd.fillRect(xR - 10, 150, widthR, 60, RGB(160,160,160));
+		
+	//Wheels	
+	lcd.fillRect(xR + 20, 160, widthR, 15, RGB(0,0,0)); //left front
+	lcd.fillRect(xR - 10, 160, widthR, 15, RGB(0,0,0)); //right front
+	lcd.fillRect(xR + 20, 190, widthR, 15, RGB(0,0,0)); //left rear
+	lcd.fillRect(xR - 10, 190, widthR, 15, RGB(0,0,0)); //right rear
+	
+	//Road
+	lcd.fillRect(xR - 20, 160, widthR, 15, RGB(160,160,160));
+	lcd.fillRect(xR - 20, 190, widthR, 15, RGB(160,160,160));
+	lcd.fillRect(xR - 20, 210, widthR, 10, RGB(160,160,160));
+
+
+}
--- car.h
+++ car.h
@@ -14,6 +14,13 @@ class Car
 		 * @param lcd The LCD to render the car on
 		 */	
 		Car(MI0283QT9 lcd);
+		void Left(MI0283QT9 lcd, uint16_t xL, uint8_t widthL);
+		void Right(MI0283QT9 lcd, uint16_t xR, uint8_t widthR);
+	private:
+		uint16_t xL;
+		uint16_t xR;
+		uint8_t widthL;
+		uint8_t widthR;
 };
 
 
