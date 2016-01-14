28193329a58a0e86b39fdf349ec8c0aac5e4288e
Danny <dzmen@hotmail.com>
2016-01-14 22:49:46 +0100

    Updated the main file with comments

--- main.ino
+++ main.ino
@@ -14,7 +14,8 @@ int main()
 	init();
 	MI0283QT9 lcd;
 	lcd.begin();
-	//TOP SECRET CODE 
+	
+	//Set lcd touchscreen calibrate values
 	lcd.tp_matrix.a = 79800;
 	lcd.tp_matrix.b = 4294966596;
 	lcd.tp_matrix.c = 4250933096;
@@ -23,10 +24,12 @@ int main()
 	lcd.tp_matrix.f = 4291263596;
 	lcd.tp_matrix.div = 109865;
 	
+	//Set the SPI clock as fast as possible
 	SPI.setClockDivider(SPI_CLOCK_DIV2);
 	
 	spel.init(lcd);
 	
+	//Run the game
 	while (1)
 	{
 		spel.run();
